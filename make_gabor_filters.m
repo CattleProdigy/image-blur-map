817178f1de96f7ab41ebd83bd74872e426afff9c
Paul Schroeder <paulschroeder90@gmail.com>
2014-03-15 17:53:46 -0400

    Finished gabor filter generation
    The filters qualitatively match the samples in the paper.
    
    They're being stored in a cell array right now. A different structure might be better. I'll evaluate this when I start using them and make changes if necessary

--- make_gabor_filters.m
+++ make_gabor_filters.m
@@ -1,8 +1,8 @@
-function t = make_gabor_filters(filter_size)
+function ti = make_gabor_filters(filter_size)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    x = [-filter_size/2:filter_size/2-1];
+    x = [-(filter_size-1)/2:filter_size/2];
     y = x;
     [xx, yy] = ndgrid(x, y);
 
@@ -14,9 +14,8 @@ function t = make_gabor_filters(filter_size)
                                 .* gaussian_function(yy, filter_size / 4, filter_size);
     
     gaussian_window = gaussian_window ./ (max(max(gaussian_window)));
-    t = gaussian_window;
 
-    gabor = @(x, y, wx, wy) exp(-2*pi*1i.*(x.*wx + y.*wy));
+    gabor = @(x, y, wx, wy, N) exp(-2*pi*1i.*(x.*wx./N + y.*wy./N));
     
     % frequency values (a1 / N
     a1 = [-(filter_size-1)/4:(filter_size-1)/4];
@@ -28,23 +27,16 @@ function t = make_gabor_filters(filter_size)
     
     ti = cell([length(a2) length(a1)]);
     for i = 1:length(a2)
-        n = length(a1);
         if (a2(i) == 0)
             for j = 1:length(a10)
-                ti{i,j} = gabor(xx, yy, a10(j), a2(i));
-                        imagesc(real(ti{i,j}));
-        pause;
+                ti{i,j} = gabor(xx, yy, a10(j), a2(i),filter_size).*gaussian_window;
             end
         else 
             for j = 1:length(a1)
-                ti{i,j} = gabor(xx, yy, a1(j), a2(i));
-                        imagesc(real(ti{i,j}));
-        pause;
+                ti{i,j} = gabor(xx, yy, a1(j), a2(i),filter_size).*gaussian_window;
             end
         end
-
     end
-    t = ti;
 end
 
 % for N = 41, 60 Filters are generated
