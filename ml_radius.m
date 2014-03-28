4f9b6b6983dd5992df183ee50bbd4bfad3ec5088
Paul J Schroeder <paulschroeder90@gmail.com>
2014-03-28 11:45:59 -0400

    Started Radius ML

--- ml_radius.m
+++ ml_radius.m
@@ -1,6 +1,23 @@
 function [s, r] = ml_radius(im, gi, sig_i_coeffs)
 
+    r_old = 1; % Initial Conditions
+    s_old = 1;
+    
+    it_max = 100;
+    
+    for i = 1:it_max
+        
+        for j = 1:size(sig_i_coeffs,1)
+            for k = 1:size(sig_i_coeffs,2)
+                if (isempty(ti{j,k}))
+                    continue;
+                end
+                rho_i{j,k} = (1 + sig_ni/(s_old*exp(polyval(sig_i_coeffs{j,k}, 
+            end
+        end
 
+        
+    end
 
 end
 
