e46d5543a2bf5bd7ed0847da8bd89bde0ea50b83
Paul J Schroeder <paulschroeder90@gmail.com>
2014-04-07 17:38:14 -0400

    Update runme to capture sig_ni

--- runme.m
+++ runme.m
@@ -8,19 +8,23 @@ tic;
 ti = make_gabor_filters(41);
 toc;
 
+gi = cell(size(ti));
+sig_ni = cell(size(ti));
 fprintf('\nCalculating Gabor Gradient Field...\n');
 tic;
-gi = gabor_gradient_field(im, ti);
+[gi sig_ni] = gabor_gradient_field(im, ti);
 toc;
 
+%%
 fprintf('\nFitting Sample PSFs...\n');
 tic;
 [sig_i_coeffs, r_domain] = fit_psf(ti);
 toc;
 
+%%
 fprintf('\nFinding most likely radii PSFs...\n');
 tic;
-[s, r] = ml_radius(im, gi, sig_i_coeffs);
+[s, r] = ml_radius(im, gi, sig_i_coeffs, sig_ni);
 toc;
 
 
