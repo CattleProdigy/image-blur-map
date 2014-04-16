e55a26d9a329166017f978416dcd78f8d0fdd0ee
Cody Hyman <codyhyman@gmail.com>
2014-04-16 11:08:45 -0400

    Completed blur test generator using random circular blur patches

--- testing/blur_generator/blurGenerator.m
+++ testing/blur_generator/blurGenerator.m
@@ -14,12 +14,23 @@ function [blurMap, noise, blurredImage] = blurGenerator(inputImage, blurSpace, n
     % Apply blur map to input images
     inputImage = double(inputImage)./255.0;
     blurMap = generateBlurMap(size(inputImage),blurSpace);
+    %oneMap = ones(size(inputImage,1),size(inputImage,2)/2);
+    %blurMap = [15 .* oneMap 3.*oneMap];
     blurredImage = blurByMap(inputImage,blurMap,blurSpace);
+    %blurredImage = blurByMap(inputImage,blurMap,[3,15]);
     noise = nvar * randn(size(inputImage)); % Generate additive white noise
     blurredImage = blurredImage + noise;    % Apply additive white noise
     
     figure(1)
+    subplot(1,2,1);
+    imagesc(inputImage);
+    axis equal;
+    colormap('gray');
+    colorbar;
+    subplot(1,2,2);
     imagesc(blurredImage);
+    axis equal;
+    colormap('gray');
     colorbar;
     figure(2);
     %imshow(blurMap./max(max(blurMap)));
@@ -29,8 +40,6 @@ end
 
 function [blurImg] = blurByMap(inputImage, blurMap, blurSpace)
     % Perform local convolution
-    xmax = size(inputImage,2);
-    ymax = size(inputImage,1);
     blurImg = zeros(size(inputImage));
     % Perform and mask multiple convolutions for fast computation 
     for i = 1:length(blurSpace)
@@ -48,12 +57,21 @@ function [blurMap] = generateBlurMap(imSize,blurSpace)
 % Generates an IID random field and then median filters it to get clusters
     xsize = imSize(2);
     ysize = imSize(1);
-    blurMap = zeros(imSize);
-    for i = 1:length(blurSpace)
-        rfield = rand(ysize,xsize);
-        disp(mat2str(i))
-        blurMap = blurMap + round(medfilt2(rfield,[20,20],'symmetric'));
+    blurMap = blurSpace(1)*ones(imSize);
+    
+    blurMap = drawCircle(blurMap, size(blurMap,2)/2, size(blurMap,1)/2,20,5);
+    for j = blurSpace
+        for k = 1:round(100*rand)
+            blurMap = drawCircle(blurMap,round(xsize*rand),round(ysize*rand),round(35*rand),j);
+        end
     end
+    % Draw random primitive geometry
+    
+%     for i = 1:length(blurSpace)
+%         rfield = rand(ysize,xsize);
+%         disp(mat2str(i))
+%         blurMap = blurMap + round(medfilt2(rfield,[10,10],'symmetric'));
+%     end
     
     %randMap = ceil(length(blurSpace).*rand(ysize,xsize));
     %figure(4);
@@ -67,4 +85,20 @@ function [blurMap] = generateBlurMap(imSize,blurSpace)
     %blurMap = medfilt2(blurMap,[30, 30],'symmetric');
     %gfilt = fspecial('gaussian',[20 20], 1);
     %blurMap = ceil(imfilter(blurMap,gfilt));
+end
+
+function [circImg] = drawCircle(inImg,xc,yc,r,value)
+% Function to draw solid value disk on image
+circImg = inImg;
+for x = (xc-r):(xc+r)
+    for y = (yc-r):(yc+r)
+        if(((x-xc).^2+(y-yc).^2) <= r.^2)
+            if((x <= size(circImg,2)) & (x >= 1))
+                if((y<= size(circImg,1)) & (y>=1))
+                    circImg(y,x) = value;
+                end
+            end
+        end
+    end
+end
 end
